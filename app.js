047b6486476fb037b9116fecabcf3847c6d645ea
fullstackdavis <johnmdavis997@gmail.com>
2019-02-13 12:36:01 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public")); // Tells express to serve the contents of the public directory
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+// Creates server
+app.listen(process.env.PORT || 3000, function (req, res) {
+    console.log("I'm not gay but dick be lookin tasty sometimes");
+});
+
